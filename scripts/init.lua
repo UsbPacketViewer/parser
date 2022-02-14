3e1f498b1e3cb5c71985c36e4a0989c6e0afffdd
victor <43309235@qq.com>
2022-02-14 22:59:11 +0800

    支持LuaPerfect

--- scripts/init.lua
+++ scripts/init.lua
@@ -1,5 +1,16 @@
 -- init.lua
 -- encoding: utf-8
+local _, LuaDebuggee = pcall(require, 'LuaDebuggee')
+if LuaDebuggee and LuaDebuggee.StartDebug then
+	if LuaDebuggee.StartDebug('127.0.0.1', 9826) then
+		print('LuaPerfect: Successfully connected to debugger!')
+	else
+		print('LuaPerfect: Failed to connect debugger!')
+	end
+else
+	print('LuaPerfect: Check documents at: https://luaperfect.net')
+end
+
 require("file_base")
 require("file_pcap")
 require("file_iti1480a")
